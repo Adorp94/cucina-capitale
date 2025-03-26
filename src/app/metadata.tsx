d79794cf40652a9d874bf02b56503ae6660a4ff3
adorp94 <rangel.ado@gmail.com>
2025-03-26 15:12:11 -0600

    modified:   src/app/client-layout.tsx 	modified:   src/app/layout.tsx 	new file:   src/app/metadata.tsx 	modified:   src/components/layout/footer.tsx 	modified:   src/components/layout/header.tsx

---
+++ src/app/metadata.tsx
@@ -0,0 +1,9 @@
+import type { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "Cucina Capitale",
+  description: "Sistema de gestión para carpintería residencial",
+  icons: {
+    icon: "/favicon.ico",
+  },
+}; 
